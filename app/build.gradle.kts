1a1086cdffbe6b603d071238d465eb7e9c4ada9f
mamusic885 <mson50502@gmail.com>
2026-09-23 12:55:46 +0530

    Update targetSdk version to 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
   defaultConfig {
     applicationId = "com.aistudio.gurbanikhoj.kpsikh"
     minSdk = 24
-    targetSdk = 35
+    targetSdk = 36
     versionCode = 1
     versionName = "1.0"
 
